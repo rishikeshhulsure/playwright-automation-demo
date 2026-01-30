200f9628e077660161cb8b4e371a1bda126157ca
Rishikesh.Hulsure <rishikesh.hulsure@rentacenter.com>
2026-01-30 09:49:30 +0530

    deleted failed test

--- tests/fail_test.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('This test will fail', async ({ page }) => {
-  await page.goto('https://example.com');
-  
-  // We expect the text "Google" to be on Example.com (It won't be there!)
-  await expect(page.getByText('Google')).toBeVisible({ timeout: 2000 });
-});
