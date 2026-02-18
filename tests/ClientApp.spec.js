344e0ecb18c598d96fd22153e02c2762030c4211
Rishikesh.Hulsure <rishikesh.hulsure@rentacenter.com>
2026-02-18 12:35:36 +0530

    modified test end to end

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -10,6 +10,7 @@ test('End to end testing', async ({page}) =>{
     await page.locator('#userPassword').fill(password);
     await page.locator('#login').click();
     await page.waitForLoadState('networkidle');
+    await products.first().waitFor();
     await expect(page.locator("div[class='card']").first()).toBeVisible();
     const count = await products.count();
     console.log(count);
@@ -20,4 +21,23 @@ test('End to end testing', async ({page}) =>{
             break;
         }
     }
+    await page.locator("[routerlink*='cart']").click();
+    await expect(page.locator("[class='infoWrap'] h3")).toHaveText(productName);
+    await page.locator("text=Checkout").click();
+    await expect(page.locator("[class*='text-validated ']")).toHaveText(userEmail);
+    await page.locator("[placeholder='Select Country']").fill('ind',{delay:100});
+    await page.locator(".ta-results").waitFor();
+    const optionsCount = await page.locator(".ta-results button").count();
+    for(let i=0; i<optionsCount; i++){
+        let text = await page.locator(".ta-results button").nth(i).textContent();
+        if(text.trim() === 'India'){
+            await page.locator(".ta-results button").nth(i).click();
+            break;
+        }    
+    }
+    await page.locator("text=Place Order").click();
+    await expect(page.locator(".hero-primary")).toHaveText('THANKYOU FOR THE ORDER.');
+    let orderNumber = await page.locator("[class='ng-star-inserted'] td label").last().textContent();
+    console.log(orderNumber);
+
 });
